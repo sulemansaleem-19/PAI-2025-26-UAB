1271e646a9cbbc913ab4202abfe134e2e6aed8a8
sulemansaleem-19 <sulemansuria15@gmail.com>
2025-10-10 18:34:06 +0200

    com

--- JS/13-setmana-4.js
+++ JS/13-setmana-4.js
@@ -1,7 +1,9 @@
-let numero = parseInt(prompt("Introdueix un numero:");
-if (numero < 0){
-    console.log("El numero llegit és negatiu");
-}
-else {
-    console.log("El numero introduit es positiu")
-}
+// Exercici 13.1
+
+// let numero = parseInt(prompt("Introdueix un numero:"));
+// if ( numero < 0){
+    // console.log("El numero es negatiu")
+// }
+// else {
+    // console.log ("EL numero es positiu")
+// }
