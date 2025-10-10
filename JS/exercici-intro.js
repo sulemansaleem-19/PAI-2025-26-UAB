4179b95a601ca2cb44662675dafe477d3bdd565f
sulemansaleem-19 <sulemansuria15@gmail.com>
2025-10-10 17:53:22 +0200

    primeros commits

---
+++ JS/exercici-intro.js
@@ -0,0 +1,22 @@
+// 2.1.- Què passaria si féssim aquestes declaracions de variables?
+// Pensa primer que creus que passaria. Després pots provar a executar aquestes instruccions
+// i veure que passa (si passa alguna cosa)
+
+// a.-
+// var a = 1; //a a= 1
+// var a = 2; // a = 2
+// console.log(a);
+
+// b.-
+var a = 1;
+let a = 2;
+console.log(a);
+// c.-
+// let a = 1;
+// var a = 2;
+// console.log(a);
+// d.-
+// let a = 1;
+// let a = 2;
+// console.log(a);
+
