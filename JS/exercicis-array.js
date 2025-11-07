c7e155cf1a589999fde060f02e177b8350449204
sulemansaleem-19 <sulemansuria15@gmail.com>
2025-11-07 18:29:55 +0100

    Update exercicis-array.js

--- JS/exercicis-array.js
+++ JS/exercicis-array.js
@@ -19,3 +19,50 @@
 // arr[6] = suma;
 // console.log(arr);
 
+// 21.4
+// let N = parseInt(prompt("Escriu un numero"));
+// let arr = [N, N*2, N*3];
+// console.log(arr);
+
+// 21.5
+// let N = parseInt(prompt("Escriu un numero"));
+// let M = parseInt(prompt("Escriu un numero"));
+// let arr = [N+M, N-M, N*M, N/M];
+// console.log(arr);
+
+// 21.6
+// let taula = [23,45,63,78,23,68,3,78,32,53,93,21,53,5,62,78];
+// console.log("Primer valor:", taula[0]);
+// console.log("Darrer valor:", taula[taula.length - 1]);
+
+// 21.7
+// let taula = [23,45,63,78,23,68,3,78,32,53,93,21,53,5,62,78];
+// for (let i = 0; i < taula.length; i++) {
+//  console.log(taula[i]);
+// }
+
+// 21.8
+// let taula = [23,45,63,78,23,68,3,78,32,53,93,21,53,5,62,78];
+// suma = 0;
+// for (let i = 0; i < taula.length; i++) {
+//    suma = suma + taula[i];
+// }
+// console.log(suma);
+
+// 21.9
+// let taula = [23,45,63,78,23,68,3,78,32,53,93,21,53,5,62,78];
+// let novaTaula = [];
+// for (let i = 0; i < taula.length; i++) {
+//  novaTaula[i] = (taula[i] + 25);
+// }
+// console.log(novaTaula);
+
+// 21.10
+
+// let taula = [23,45,63,78,23,68,3,78,32,53,93,21,53,5,62,78];
+// let suma = 0;
+// for (let i = 0; i < taula.length; i++) {
+//  suma = suma + taula[i];
+// }
+// let mitjana = suma / taula.length;
+// console.log(mitjana);
