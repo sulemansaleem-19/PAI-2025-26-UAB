4448b410d27a2e987154996d0c7832f7725d7ef4
sulemansaleem-19 <sulemansuria15@gmail.com>
2025-11-07 18:04:43 +0100

    Update exercicis-array.js

--- JS/exercicis-array.js
+++ JS/exercicis-array.js
@@ -4,3 +4,12 @@
 // arr[1] = "Adeu";
 // console.log(arr);
 
+// 21.2
+// let arr = [];
+// arr[0] = "Hola" ;
+// arr[1] = "Adeu";
+// console.log(arr);
+// arr[2] = "NOM";
+// arr[1] = "," ;
+// console.log(arr);
+
