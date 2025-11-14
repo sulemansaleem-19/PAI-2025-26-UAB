7b870f388b96a3c4bdb9bd834989518400b9012b
sulemansaleem-19 <sulemansuria15@gmail.com>
2025-11-14 17:50:06 +0100

    exercicis

---
+++ JS/Exercicis-Funcions.js
@@ -0,0 +1,31 @@
+// Exercicis Funcions 1
+// 3.1
+// function pareellSenar(num) {
+//   let resultat = "";
+//   if (num % 2 === 0) {
+//     resultat = "El nombre és parell";
+//   } else {
+//     resultat = "El nombre és senar";
+//   }
+//   return resultat;
+// }
+
+// let numEntrada = parseInt(prompt("Escriu un número"));
+// let resultat = pareellSenar(numEntrada);
+// console.log(resultat); 
+
+// 3.2
+
+function traspas(any) {
+  let resultat = "";
+  if ((any % 4 === 0 && any % 100 !== 0) || (any % 400 === 0)) {
+    resultat = "El any es de traspas";
+  } else {
+    resultat = "El any no es de traspas";
+  }
+  return resultat;
+}
+
+let anyEntrada = parseInt(prompt("Escriu un número"));
+let resultat = traspas(anyEntrada);
+console.log(resultat); 
