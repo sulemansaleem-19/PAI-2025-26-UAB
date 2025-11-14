8a360a7c55060e0142f20fa0a2d8e20096072635
sulemansaleem-19 <sulemansuria15@gmail.com>
2025-11-14 18:16:22 +0100

    Update Exercicis-Funcions.js

--- JS/Exercicis-Funcions.js
+++ JS/Exercicis-Funcions.js
@@ -32,17 +32,52 @@
 
 //3.3
 
-function equacion(N) {
-  let suma = 0;
-  let result = 0;
-  for (let n = 1; n <= N; n = n + 1) {
-    result = 1 / (n * n);
-    suma = suma + result;
-  }
-  return suma;
-}
+// function equacion(N) {
+//   let suma = 0;
+//   let result = 0;
+//   for (let n = 1; n <= N; n = n + 1) {
+//     result = 1 / (n * n);
+//     suma = suma + result;
+//   }
+//   return suma;
+// }
 
-let Nentrada = parseInt(prompt("Introdueix un enter N"));
-let resultat = equacion(Nentrada);
-console.log(resultat);
+// let Nentrada = parseInt(prompt("Introdueix un enter N"));
+// let resultat = equacion(Nentrada);
+// console.log(resultat);
+
+// 3.4
+
+// function Nota(notaAC, notaPract, notaExamen) {
+//   let notaFinal = 0;
+//   let lletra = "";
+ 
+//   if (notaAC >= 5) {
+//     notaFinal = 0.2 * notaAC + 0.4 * notaPract + 0.4 * notaExamen;
+//   } else {
+//     notaFinal = 0.4 * notaPract + 0.6 * notaExamen;
+//   }
+
+//   if (notaFinal < 5) {
+//     lletra = "D";
+//   } else if (notaFinal >= 5 && notaFinal < 6.5) {
+//     lletra = "C";
+//   } else if (notaFinal >= 6.5 && notaFinal < 8) {
+//     lletra = "B";
+//   } else if (notaFinal >= 8 && notaFinal < 10) {
+//     lletra = "A";
+//   } else if (notaFinal === 10) {
+//     lletra = "H";
+//   }
+
+//   return [notaFinal, lletra];
+// }
+
+// let notaAC = parseFloat(prompt("Introdueix la nota d'avaluació continuada"));
+// let notaPract = parseFloat(prompt("Introdueix la nota de pràctiques"));
+// let notaExamen = parseFloat(prompt("Introdueix la nota de l'examen"));
+
+// let resultat = Nota(notaAC, notaPract, notaExamen);
+// console.log(resultat);
+
 
